e10bd654133cedbb08921856728f1f5ece2b5d25
Lebenji276 <benjamin.gille@gmail.com>
2019-03-27 17:54:41 +0100

    AJout nom Départemet

---
+++ PhoneAppPPE/Departement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace PhoneAppPPE
+{
+    class Departement
+    {
+        public string nom { get; set; }
+        public string code { get; set; }
+        public string codeRegion { get; set; }
+
+        public override string ToString() => $"Département : {nom} ({code})";
+
+    }
+}
--- PhoneAppPPE/MainActivity.cs
+++ PhoneAppPPE/MainActivity.cs
@@ -14,10 +14,12 @@ namespace PhoneAppPPE
     public class MainActivity : AppCompatActivity
     {
         private Dictionary<string, Dictionary<string, Praticien>> DicoFinal = new Dictionary<string, Dictionary<string, Praticien>>();
+        private Dictionary<string, string> Departement = new Dictionary<string, string>();
 
         private ExpandableListViewAdapter mAdapter;
         private ExpandableListView expandableListView;
         private List<string> group = new List<string>();
+        private List<string> groupAff = new List<string>();
         private Dictionary<string, List<string>> dicMyMap = new Dictionary<string, List<string>>();
         protected override void OnCreate( Bundle bundle )
         {
@@ -28,10 +30,10 @@ namespace PhoneAppPPE
             expandableListView = FindViewById<ExpandableListView>(Resource.Id.expandableListView);
             expandableListView.SetAdapter(mAdapter);
 
+            SetData(out mAdapter);
             //Set Data
             try
             {
-                SetData(out mAdapter);
             }
             catch (System.Exception)
             {
@@ -89,9 +91,35 @@ namespace PhoneAppPPE
                 }
             }
 
+            HttpWebRequest webRequest2 = WebRequest.Create("https://geo.api.gouv.fr/departements") as HttpWebRequest;
+            if (webRequest2 == null)
+            {
+                return;
+            }
+            webRequest2.ContentType = "application/json";
+            webRequest2.UserAgent = "Nothing";
+
+            using (Stream s = webRequest2.GetResponse().GetResponseStream())
+            {
+                using (StreamReader sr = new StreamReader(s))
+                {
+
+                    string contributorsAsJson = sr.ReadToEnd();
+                    List<Departement> contributors = JsonConvert.DeserializeObject<List<Departement>>(contributorsAsJson);
+                    foreach (Departement item in contributors)
+                    {
+                        Departement.Add(item.code, item.nom);
+                    }
+                }
+            }
+
             foreach (string item in ListCP)
             {
                 group.Add(item);
+
+                string nomDepartement = Departement[item];
+                groupAff.Add("Département : " + nomDepartement + "(" + item + ")");
+
             }
 
             foreach (string item in group)
@@ -99,7 +127,7 @@ namespace PhoneAppPPE
                 add_dicMyMap(item);
             }
 
-            mAdapter = new ExpandableListViewAdapter(this, group, dicMyMap);
+            mAdapter = new ExpandableListViewAdapter(this, groupAff, dicMyMap);
         }
         public void add_dicMyMap( string dep )
         {
@@ -109,7 +137,7 @@ namespace PhoneAppPPE
             {
                 groupB.Add(item.Value.ToString());
             }
-            dicMyMap.Add(group[index], groupB);
+            dicMyMap.Add(groupAff[index], groupB);
         }
     }
 }

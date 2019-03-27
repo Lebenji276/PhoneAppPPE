5dc8d1ace8a96fc5fcfce4ac2c0a4f73c6eb2890
Lebenji276 <benjamin.gille@gmail.com>
2019-03-27 11:29:06 +0100

    Fin dev

---
+++ PhoneAppPPE/Visiteur.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace PhoneAppPPE
+{
+    class Visiteur
+    {
+        private string _MinCP;
+        public string Id { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Adresse { get; set; }
+        public string Cp { get; set; }
+        public string Ville { get; set; }
+        public string IDAPI { get; set; }
+        public string MinCP
+        {
+            get
+            {
+                string PremierCp = Cp[0] + "";
+                string DeuxCP = Cp[1] + "";
+                return PremierCp + DeuxCP;
+            }
+            set => _MinCP = value;
+        }
+        public override string ToString() => $"{Nom.ToUpper()} {Prenom}\n{Adresse}\n{Cp}\t{Ville}";
+    }
+}
